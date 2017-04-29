f99588f4657559e4ac8b912c204f5bef908a4158
Seling <seling.chen@yahoo.com>
2017-04-29 12:10:21 -0700

    push notifications work when pushing from currentlocation!!

--- functions/index.js
+++ functions/index.js
@@ -1,67 +1,55 @@
-var functions = require('firebase-functions');
-
-// // Start writing Firebase Functions
-// // https://firebase.google.com/preview/functions/write-firebase-functions
-// 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// })
-var admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 var request = require('request');
-
 var API_KEY = "AAAAMyiwRVg:APA91bF0hCjJgtiBzw0CRyte0gJ3lEUJPYYGAfBnvluPFIThMNF5VoV2Tdz7tvDSYM20v9J0u9Arw7tLZLldScIgJvAtbozIk1CDYXfT_tbqpHig1B9nziXmr8b7WHZeA6XxKG3HsrS3"; // Your Firebase Cloud Messaging Server API key
+require('@google-cloud/debug-agent').start({ allowExpressions: true });
 
-// Fetch the service account key JSON file contents
-var serviceAccount = require("./serviceAccount.json");
+admin.initializeApp(functions.config().firebase);
 
-// Initialize the app with a service account, granting admin privileges
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://zero-5e2ff.firebaseio.com"
-});
+exports.sendNotificationToLocation = functions.database.ref("notificationRequests/{notificationID}").onWrite(event =>{
+	if(event.data.val()){
+		var location = event.data.val().location;
+		var message = event.data.val().message;
+		// console.log(location);
+		// console.log(message);
+		// request({
+		// 	url: 'https://fcm.googleapis.com/fcm/send',
+		// 	method: 'POST',
+		// 	headers: {
+		// 		'Content-Type' :' application/json',
+		// 		'Authorization': 'key='+API_KEY
+		// },
+		// body: JSON.stringify({
+		// 	notification: {
+		// 		title: message
+		// 	},
+		// 	to : '/topics/'+location
+		// })
+		// }, function(error, response, body) {
+		// 	if (error) { console.error(error); }
+		// 	else if (response.statusCode >= 400) { 
+		// 		console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+		// 	}
+		// 	else {
+				
+		// 	}
+		// });
 
-ref = admin.database().ref();
-
-function listenForNotificationRequests() {
-	var requests = ref.child('notificationRequests');
-	requests.on('child_added', function(requestSnapshot) {
-		var request = requestSnapshot.val();
-		sendNotificationToTopic(
-			request.location, 
-			request.message,
-			function() {
-				requestSnapshot.ref.remove();
+		const payload = {
+			notification: {
+				title: message
 			}
-		);
-	}, function(error) {
-		console.error(error);
-	});
-};
+		};
 
-function sendNotificationToTopic(location, message, onSuccess) {
-	request({
-		url: 'https://fcm.googleapis.com/fcm/send',
-		method: 'POST',
-		headers: {
-			'Content-Type' :' application/json',
-			'Authorization': 'key='+API_KEY
-	},
-	body: JSON.stringify({
-		notification: {
-			title: message
-		},
-		to : '/topics/'+location
-	})
-	}, function(error, response, body) {
-		if (error) { console.error(error); }
-		else if (response.statusCode >= 400) { 
-			console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+		admin.messaging().sendToTopic(location, payload)
+			.then(function(response) {
+				// See the MessagingDeviceGroupResponse reference documentation for
+				// the contents of response.
+				console.log("Successfully sent message:", response);
+				event.data.ref.remove();
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
 		}
-		else {
-		 	onSuccess();
-		}
-	});
-}
-
-// start listening
-listenForNotificationRequests();
+});
---
+++ functions/index2.js
@@ -0,0 +1,62 @@
+var functions = require('firebase-functions');
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/preview/functions/write-firebase-functions
+// 
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// })
+var admin = require('firebase-admin');
+var request = require('request');
+
+var API_KEY = "AAAAMyiwRVg:APA91bF0hCjJgtiBzw0CRyte0gJ3lEUJPYYGAfBnvluPFIThMNF5VoV2Tdz7tvDSYM20v9J0u9Arw7tLZLldScIgJvAtbozIk1CDYXfT_tbqpHig1B9nziXmr8b7WHZeA6XxKG3HsrS3"; // Your Firebase Cloud Messaging Server API key
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./serviceAccount.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://zero-5e2ff.firebaseio.com"
+});
+
+ref = admin.database().ref();
+	var requests = ref.child('notificationRequests');
+	requests.on('child_added', function(requestSnapshot) {
+		var request = requestSnapshot.val();
+		sendNotificationToTopic(
+			request.location, 
+			request.message,
+			function() {
+				requestSnapshot.ref.remove();
+			}
+		);
+	}, function(error) {
+		console.error(error);
+	});
+
+function sendNotificationToTopic(location, message, onSuccess) {
+	request({
+		url: 'https://fcm.googleapis.com/fcm/send',
+		method: 'POST',
+		headers: {
+			'Content-Type' :' application/json',
+			'Authorization': 'key='+API_KEY
+	},
+	body: JSON.stringify({
+		notification: {
+			title: message
+		},
+		to : '/topics/'+location
+	})
+	}, function(error, response, body) {
+		if (error) { console.error(error); }
+		else if (response.statusCode >= 400) { 
+			console.error('HTTP Error: '+response.statusCode+' - '+response.statusMessage); 
+		}
+		else {
+		 	onSuccess();
+		}
+	});
+}
+
